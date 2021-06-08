5c711565385cfb444de8ff0977cc7c2c1132078f
Tim Austin <tim@neenjaw.com>
2021-06-07 20:58:43 -0600

    pared down

--- src/events/command.ts
+++
@@ -1,8 +0,0 @@
-import { App } from '@slack/bolt'
-
-export function registerCommandEvents(app: App) {
-  app.command('/prbot', async ({ command, ack, say }) => {
-    await ack()
-    await say(`${command.text}`)
-  })
-}
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,25 @@
-import fs from 'fs'
-import path from 'path'
+import { Octokit } from 'octokit'
 
-if (process.env.NODE_ENV !== 'production') {
-  const envPath = path.join(__dirname, '../.env')
-  if (fs.existsSync(envPath)) {
-    console.log(envPath + ' exists')
-  } else {
-    console.log(envPath + ' does not exists')
+export const fetchPRs = async (): Promise<string> => {
+  const octokit = process.env.GITHUB_TOKEN
+    ? new Octokit({ auth: process.env.GITHUB_TOKEN })
+    : undefined
+
+  if (!octokit) {
+    throw new Error('Octokit is uninitialized')
   }
-  require('dotenv').config({ path: envPath })
-}
 
-import { App, ExpressReceiver, LogLevel } from '@slack/bolt'
-import { registerCommandEvents } from './events/command'
-import { registerMessageEvents } from './events/message'
-import { registerViewEvents } from './events/view'
-
-import type { HttpFunction } from '@google-cloud/functions-framework/build/src/functions'
-
-if (!process.env.GITHUB_TOKEN) {
-  throw new Error('Github token must be supplied in `GITHUB_TOKEN` env var')
-}
-if (!process.env.SLACK_BOT_TOKEN) {
-  throw new Error(
-    'Slack bot token must be present in the `SLACK_BOT_TOKEN` env var'
+  const {
+    viewer: { login },
+  } = await octokit.graphql(
+    `
+      {
+        viewer {
+          login
+        }
+      }
+    `
   )
+
+  return login
 }
-if (!process.env.SLACK_SIGNING_SECRET) {
-  throw new Error(
-    'Slack signing secret must be present in the `SLACK_SIGNING_SECRET` env var'
-  )
-}
-
-const receiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-})
-
-const app = new App({
-  receiver,
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  logLevel: LogLevel.DEBUG,
-})
-
-registerCommandEvents(app)
-registerMessageEvents(app)
-registerViewEvents(app)
-
-export const prBot: HttpFunction = receiver.app
